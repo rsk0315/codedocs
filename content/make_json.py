c2b31ecf2848638109879dffcbe2e188d1e6037f
rsk0315 <rsk0315@icloud.com>
2019-10-05 18:51:42 +0900

    Update JSON format

--- content/make_json.py
+++ content/make_json.py
@@ -1,25 +1,40 @@
+import configparser
 import glob
 import json
+import os.path
 
-def main():
-    files = {}
+def make_json():
+    index = {}
 
     for f in sorted(glob.glob('*/*.md')):
         dirname, basename = f.split('/')
         root, ext = basename.rsplit('.')
-        if dirname not in files:
-            files[dirname] = []
+        if dirname not in index:
+            index[dirname] = {'contents': []}
 
         with open(f) as fin:
             title = fin.readline().lstrip('% ').rstrip()
 
-        files[dirname].append({root: title})
+        index[dirname]['contents'].append({'id': root, 'title': title})
 
-    for dirname in files:
-        files[dirname].sort(key=lambda x: list(x.keys())[0])
+    for dirname in index:
+        index[dirname]['contents'].sort(key=lambda x: x['id'])
 
-    with open('index.json', 'w') as fout:
-        json.dump(files, fout, indent=4)
+        index_file = f'{dirname}/.config'
+        if not os.path.isfile(index_file):
+            index[dirname]['title'] = dirname
+            continue
+
+        cfp = configparser.ConfigParser()
+        cfp.read(index_file)
+        index[dirname]['title'] = cfp['INDEX']['title']
+
+    print(json.dumps(index, indent=4))
+    # with open('index.json', 'w') as fout:
+    #     json.dump(index, fout, indent=4)
+
+def main():
+    make_json()
 
 if __name__ == '__main__':
     main()
